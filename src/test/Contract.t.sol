12eab4d513575cd90eee09fdb057d5c29de705e6
x3devships <x3dev@protonmail.com>
2022-02-07 09:21:26 -0600

    boilerplate

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -2,9 +2,14 @@
 pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
+import "./Vm/CheatCodes.sol";
+import "./console.sol";
 
 contract ContractTest is DSTest {
-    function setUp() public {}
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    function setUp() public {
+        console.log("aaaaaaah i'm setting up");
+    }
 
     function testExample() public {
         assertTrue(true);
