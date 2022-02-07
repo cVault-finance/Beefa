38978786cf0dee75dcaf0a69559163d3fc9329db
x3devships <x3dev@protonmail.com>
2022-02-07 16:27:25 -0600

    notes added for txes

--- README.md
+++ README.md
@@ -4,3 +4,66 @@ This repo will become the multisig test system for the coreDAO. Add your proposa
 In your proposal please provide on-chain output as a markdown file with adequate illustrative value for non-technical voters.
 
  - x3
+
+
+---
+
+cLean Feb 7
+
+ - [Remove 29.258102080375407427 CORE And 2.96455998 cBTC Liquidity From Uniswap V2](https://etherscan.io/tx/0x12d090c79e6c58c07a769eb89cbfd2b8a540e9eb5ede476a16613d4fef8b6891)
+  
+    + 29.2581 CORE = 29.2581 CORE
+    + 2.96455998 cBTC = 2.96455998 cBTC
+    0 coreDAI
+    0 wCore
+    0 WBTC
+ 
+ - [Sell 12 CORE in to cBTC liquidity for 0.61097737 cBTC](https://etherscan.io/tx/0x410a7f9e2d1c7a4799edfd1cc63b37a0d45353f8be116bfe95f885248f2733c6)
+
+    - 12 CORE = 17.2581 CORE
+    + 0.61097737 cBTC = 3.57553735 cBTC
+    0 coreDAI
+    0 wCore
+    0 WBTC
+
+ - [Remove 71,228.037874790909058162 coreDAI And 11.23658861426748242 wCORE Liquidity From Uniswap V2](https://etherscan.io/tx/0xd206489b47e934e1eb7fde4f6e77a39e04e61978b681d0e49797cfd143746f7a)
+
+    17.2581 CORE
+    3.57553735 cBTC
+    + 71228 coreDAI = 71228 coreDAI
+    + 11.2365 wCORE = 11.2365 wCORE
+    0 WBTC
+    
+
+ - [Swap 9 wCORE For 29,233.739062516014730363 coreDAI On Uniswap V2](https://etherscan.io/tx/0x63a7bbd7c668e7cf0f0dcff3be970a5f9b054e8b1a41c22b950aca5393712233)
+
+    17.2581 CORE
+    3.57553735 cBTC
+    + 29233 coreDAI = 100461 coreDAI
+    - 9 wCORE = 2.2365 wCORE
+    0 WBTC
+    
+
+ - [Swap 2.23658861426748242 wCORE For 3,327.126671913704056625 coreDAI OnUniswap V2](https://etherscan.io/tx/0x00ccc44808be0c026d1b04aad96d4a68c7437feb37e6769fab963e51bc4d066c)
+
+    17.2581 CORE
+    3.57553735 cBTC
+    + 3327 coreDAI = 103788 coreDAI
+    - 2.2365 wCORE = 0 wCORE
+    0 WBTC
+
+- [Unwrap 3.57553735 cBTC for 3.57553735 WBTC](https://etherscan.io/tx/0xfad1318e3aedde02c376289cc9bead37f26404da5af7c84c7ad1a48594b16d8b)
+
+    17.2581 CORE
+    -3.57553735 cBTC = 0 cBTC
+    103788 coreDAI
+    0 wCORE
+    +3.57553735 WBTC = 3.57553735 WBTC
+
+### Net change
+
+    +17.2581 CORE
+    +103,788 coreDAI
+    +3.57553735 WBTC
+    0 cBTC
+    0 wCORE
--- src/test/testCLeanup.sol
+++ src/test/testCLeanup.sol
@@ -3,25 +3,45 @@ pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
 import "./testlibs/CheatCodes.sol";
-import "./testlibs/Etcher.sol";
 import "./console.sol";
 import "../IUniswapV2Pair.sol";
+import "../cLeanCBTC.sol";
+
+interface COREDEXToken {
+    function balanceOf(address) external returns (uint256);
+}
 
 contract ContractTest is DSTest {
     address beefa = 0xB2d834dd31816993EF53507Eb1325430e67beefa;
     address deployer = 0x5A16552f59ea34E44ec81E58b3817833E9fD5436;
     IUniswapV2Pair coreCbtcPair = IUniswapV2Pair(0x6fad7D44640c5cd0120DEeC0301e8cf850BecB68);
     Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
-    Etcher etcher = new Etcher(vm);
+    COREDEXToken cbtcToken = COREDEXToken(0x7b5982dcAB054C377517759d0D2a3a5D02615AB8);
+
     function setUp() public {
         console.log("aaaaaaah i'm cleaning up");
     }
 
     function testBurnLP() public {
-        uint256 startBal = coreCbtcPair.balanceOf(beefa);
+        uint256 MAX_INT = 2**256 - 12;
+        uint256 startcBTCBal = cbtcToken.balanceOf(deployer);
+        CLeanCBTC cleanCBTC = new CLeanCBTC();
+        uint256 endcBTCBal = cbtcToken.balanceOf(deployer);
+        console.log("startcBTCBal:",startcBTCBal);
+        vm.startPrank(deployer, deployer);
+        console.log("111:",startcBTCBal);
+        console.log("approve", address(cleanCBTC));
+        coreCbtcPair.approve(address(cleanCBTC), startcBTCBal);
+        // coreCbtcPair.approve(address(coreCbtcPair), MAX_INT);
+        console.log("222:",startcBTCBal);
+        cleanCBTC.clean();
+        console.log("333:",startcBTCBal);
+        console.log("endcBTCBal:",endcBTCBal);
+        /*uint256 startBal = coreCbtcPair.balanceOf(beefa);
+        console.log("startbal: ", startBal);
         vm.startPrank(beefa);
         console.log("aaaaaah i'm transferring LP to the pair contract");
-        coreCbtcPair.approve(deployer, startBal);
+        // coreCbtcPair.approve(deployer, startBal);
 
         console.log("transfering to deployer...");
         coreCbtcPair.transfer(deployer, startBal);
@@ -37,7 +57,7 @@ contract ContractTest is DSTest {
         coreCbtcPair.burn(beefa);
         console.log("aaaaaah i burned my LP tokens");
         uint256 endBal = coreCbtcPair.balanceOf(beefa);
-        assert(startBal > endBal);
+        assert(startBal > endBal);*/
         assertTrue(true);
     }
 }
