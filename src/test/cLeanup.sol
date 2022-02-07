efb0b58ae55d81cd57906bf7ab6be2cb86d7d00f
x3devships <x3dev@protonmail.com>
2022-02-07 10:55:37 -0600

    aaaaah

--- src/test/cLeanup.sol
+++ src/test/cLeanup.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
 import "./testlibs/CheatCodes.sol";
+import "./testlibs/Etcher.sol";
 import "./console.sol";
 import "../IUniswapV2Pair.sol";
 
@@ -10,6 +11,7 @@ contract ContractTest is DSTest {
     address beefa = 0xB2d834dd31816993EF53507Eb1325430e67beefa;
     IUniswapV2Pair coreCbtcPair = IUniswapV2Pair(0x6fad7D44640c5cd0120DEeC0301e8cf850BecB68);
     Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    Etcher etcher = new Etcher(vm);
     function setUp() public {
         console.log("aaaaaaah i'm cleaning up");
     }
@@ -17,7 +19,11 @@ contract ContractTest is DSTest {
     function testBurnLP() public {
         uint256 startBal = coreCbtcPair.balanceOf(beefa);
         vm.startPrank(beefa);
+        console.log("aaaaaah i'm transferring LP to the pair contract");
+        coreCbtcPair.transfer(address(coreCbtcPair), coreCbtcPair.balanceOf(beefa));
+        console.log("aaaaaah i'm burning my LP tokens");
         coreCbtcPair.burn(beefa);
+        console.log("aaaaaah i burned my LP tokens");
         uint256 endBal = coreCbtcPair.balanceOf(beefa);
         assert(startBal > endBal);
         assertTrue(true);
---
+++ src/test/testlibs/Etcher.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import "./CheatCodes.sol";
+
+contract Etcher {
+    Vm vm;
+    constructor(Vm _vm) {
+        vm = _vm;
+    }
+    
+}
